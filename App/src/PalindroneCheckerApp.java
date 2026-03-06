7d9ad888fdb54078328c1fe4a9b293ce80dab0b0
devcrazy2812 <devcrazy2812@gmail.com>
2026-03-06 14:49:57 +0530

    Abhay Added UC10-WelcomePage

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,29 @@
-public class PalindroneCheckerApp {
+public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" Welcome to Palindrome Checker App ");
+        // UC10: Case-Insensitive & Space-Ignored Palindrome Check
+        String input = "A man a plan a canal Panama";
+
+
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+        int start = 0;
+        int end = normalized.length() - 1;
+        boolean isPalindrome = true;
+
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Normalized : " + normalized);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
