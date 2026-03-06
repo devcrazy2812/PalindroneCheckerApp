8343cf70c8a00329b6b1ed58dad107720b6a49cd
devcrazy2812 <devcrazy2812@gmail.com>
2026-03-06 14:35:35 +0530

    Abhay Added UC6-WelcomePage

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,31 +1,36 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC5: Stack-Based Palindrome Checker
-        String input = "noon";
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
 
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
-        for (char c : input.toCharArray()) {
-            stack.push(c);
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
 
         boolean isPalindrome = true;
 
-        // Pop and compare
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        // Print result
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        if (isPalindrome) {
+            System.out.println("UC6 Result: " + input + " is a palindrome.");
+        } else {
+            System.out.println("UC6 Result: " + input + " is NOT a palindrome.");
+        }
     }
 }
